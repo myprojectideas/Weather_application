0e8159d28b8d422c80cdb6ec21b14b18213df564
engineer2u4u <mohit.ashliya@gmail.com>
2023-07-05 22:47:18 +0530

    first commit

---
+++ src/Context/index.jsx
@@ -0,0 +1,65 @@
+import { useContext, createContext, useState, useEffect } from "react";
+import axios from 'axios'
+
+const StateContext = createContext()
+
+export const StateContextProvider = ({ children }) => {
+    const [weather, setWeather] = useState({})
+    const [values, setValues] = useState([])
+    const [place, setPlace] = useState('Jaipur')
+    const [thisLocation, setLocation] = useState('')
+
+    // fetch api
+    const fetchWeather = async () => {
+        const options = {
+            method: 'GET',
+            url: 'https://visual-crossing-weather.p.rapidapi.com/forecast',
+            params: {
+                aggregateHours: '24',
+                location: place,
+                contentType: 'json',
+                unitGroup: 'metric',
+                shortColumnNames: 0,
+            },
+            headers: {
+                'X-RapidAPI-Key': import.meta.env.VITE_API_KEY,
+                'X-RapidAPI-Host': 'visual-crossing-weather.p.rapidapi.com'
+            }
+        }
+
+        try {
+            const response = await axios.request(options);
+            console.log(response.data)
+            const thisData = Object.values(response.data.locations)[0]
+            setLocation(thisData.address)
+            setValues(thisData.values)
+            setWeather(thisData.values[0])
+        } catch (e) {
+            console.error(e);
+            // if the api throws error.
+            alert('This place does not exist')
+        }
+    }
+
+    useEffect(() => {
+        fetchWeather()
+    }, [place])
+
+    useEffect(() => {
+        console.log(values)
+    }, [values])
+
+    return (
+        <StateContext.Provider value={{
+            weather,
+            setPlace,
+            values,
+            thisLocation,
+            place
+        }}>
+            {children}
+        </StateContext.Provider>
+    )
+}
+
+export const useStateContext = () => useContext(StateContext)
---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import { StateContextProvider } from './Context/index.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <StateContextProvider>
+    <App />
+  </StateContextProvider>,
+)
